9540dc95b367d66c033725d30ea37602117923fb
Bryan <bryanlima200@outlook.com>
2022-03-13 18:13:13 -0300

    Alterado FreteController para exigir autenticação

--- SkyCommerce/SkyCommerce.Frete/Controllers/FreteController.cs
+++ SkyCommerce/SkyCommerce.Frete/Controllers/FreteController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using SkyCommerce.Fretes.Context;
@@ -11,7 +12,7 @@ using System.Threading.Tasks;
 namespace SkyCommerce.Fretes.Controllers
 {
     [ApiController]
-    [Route("fretes")]
+    [Route("fretes"), Authorize]
     public class FreteController : ControllerBase
     {
         private readonly ILogger<FreteController> _logger;
